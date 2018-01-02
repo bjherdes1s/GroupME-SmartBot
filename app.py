379b3c889009e6dc206181b6699d61335eaf8d11
root <root@localhost.localdomain>
2018-01-02 15:25:56 -0600

    initial

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import os
 import sys
 import json
+#import curlOn
+#import curlOn
+#import requests
 
 from urllib.parse import urlencode
 from urllib.request import Request, urlopen
@@ -19,9 +22,11 @@ def webhook():
   # We don't want to reply to ourselves! A infinite loop is a result if 
   #bot replies to itself
   if data['name'] != 'TestBot':
+#      if data['text'] == 'Activate'
      msg = '{}, you sent "{}".'.format(data['name'], data['text'])
      send_message(msg)
-
+#     func1()
+ #    curlOn.func1()
   return "ok", 200
 
 def send_message(msg):
@@ -33,7 +38,22 @@ def send_message(msg):
          }
   request = Request(url, urlencode(data).encode())
   json = urlopen(request).read().decode()
-  
+
 def log(msg):
   print(str(msg))
   sys.stdout.flush()
+
+#def func1():
+ # headers = {
+  #  'Content-Type': 'application/json',
+#  }
+
+ # params = (
+  #  ('token', '93543fb3-71c246da834245d488b8a17'),
+ # )
+
+ # data = '{"method":"passthrough", "params": {"deviceId": "8006C228155C6C83E12C0B0706A5EC18184C53CE", "requestData": "{\\"system\\":{\\"set_relay_state\\":{\\"state\\":1}}}" }}'
+
+ # response = requests.post('https://use1-wap.tplinkcloud.com/', headers=headers, params=params, data=data)
+
+
---
+++ curlOff.py
@@ -0,0 +1,14 @@
+import requests
+
+headers = {
+    'Content-Type': 'application/json',
+}
+
+params = (
+    ('token', '93543fb3-71c246da834245d488b8a17'),
+)
+
+data = '{"method":"passthrough", "params": {"deviceId": "8006C228155C6C83E12C0B0706A5EC18184C53CE", "requestData": "{\\"system\\":{\\"set_relay_state\\":{\\"state\\":0}}}" }}'
+
+response = requests.post('https://use1-wap.tplinkcloud.com/', headers=headers, params=params, data=data)
+
