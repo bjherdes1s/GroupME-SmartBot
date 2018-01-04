0692bdc8f5ee6c5217d0b803148e39df8c5b3d3e
root <root@localhost.localdomain>
2018-01-04 00:26:42 -0600

    Initial

--- README.md
+++ README.md
@@ -2,13 +2,13 @@
 
 ## Introduction
 I set out to become more familiar with JSON, specifically POST requests containing JSON data to the 
-GroupME and TP-Link API's. The GroupME bot was the easy part. There is an online form at the [GroupME Developers Page](dev.groupme.com/bots) where you can even create
+GroupME and TP-Link API's. The GroupME bot was the easy part. There is an online form at https://dev.groupme.com/bots where you can create
 your own bot easily and assign it to a group of your choosing. From here, your bot can be assigned to a callback server. This callback
 server should process chats and trigger the HS100 smart plug on or off, depending on the chat message received.
 
 To be used in conjuction with Heroku and deployed with git Heroku tools. 
 Without a hosting platform or port forwarding on your own home web server,
-your capabilities will be minimal with a groupme bot. 
+your capabilities will be minimal with this GroupME SmartBot. 
 
 ### Hardware/Software Necessary
 
@@ -24,9 +24,16 @@ your capabilities will be minimal with a groupme bot.
 3. Further action, in my case, is the turning 'on' or 'off' of the TP-Link HS100 smart plug. 
 4. Logs of the API calls should be visible in the Heroku App management dashboard.
 
-*For a full implementation of this project, the GROUPME and TP-Link REST API's must be utilized, 
-specifically for the Kasa App-like POST request.*
+##DEMO
 
+*Also see the screenshots folder I am including for convenience.*
 
+###Make it your own
+1. Change any mention of 'YOUR_BOT_ID_HERE' to your GroupME bot's ID. Another option is to make a heroku config environment variable.
+2. Change any mention of 'YOUR_TOKEN_HERE' to your TP-Link access token normally used by the Kasa app. You may have to extract these from a .plist or .db file from the Kasa mobile application.
+3. Change any mention of 'YOUR_DEVICEID_HERE' to your smartplug DeviceID. You can find this in your Kasa app's .db file or with an appropriate cURL POST.
+
+*I have included the curlOn.py and curlOff.py scripts, which are just func1() and func2() from the app.py. Having these separately may help in troubleshooting 
+the conversation between heroku hosted app and the TP-Link REST API.*
 
 Contact: [brytonjherdes@gmail.com]
--- app.py
+++ app.py
@@ -16,23 +16,26 @@ def webhook():
   data = request.get_json()
   log('Recieved {}'.format(data))
 
-  # We don't want to reply to ourselves! A infinite loop is a result if 
-  #bot replies to itself
+  # We don't want to reply to ourselves! A infinite loop is a result if
+  #bot replies to itself. 
   if data['name'] != 'TestBot' and data['text'] == 'on':
-     #msg = '{}, you sent "{}".'.format(data['name'], data['text'])
+     #Light turned on GroupME message
      msg = 'Light has been turned on'
      send_message(msg)
+     #Turn Light On
      func1()
   if data['name'] != 'TestBot' and data['text'] == 'off':
-    msg = 'Light has been turned off'
+    #Light turned off GroupME message
+     msg = 'Light has been turned off'
     send_message(msg)
+    #Turn Light Off
     func2()
 
 def send_message(msg):
   url  = 'https://api.groupme.com/v3/bots/post'
 
   data = {
-          'bot_id' : "5e9407c447fbbc988ca2eb7076",
+          'bot_id' : "YOUR_BOT_ID_HERE",
           'text'   : msg,
          }
   request = Request(url, urlencode(data).encode())
@@ -46,10 +49,10 @@ def func1():
   }
 
   params = (
-    ('token', '93543fb3-71c246da834245d488b8a17'),
+    ('token', 'YOUR_TOKEN_HERE'),
   )
 
-  data = '{"method":"passthrough", "params": {"deviceId": "8006C228155C6C83E12C0B0706A5EC18184C53CE", "requestData": "{\\"system\\":{\\"set_relay_state\\":{\\"state\\":1}}}" }}'
+  data = '{"method":"passthrough", "params": {"deviceId": "YOUR_DEVICEID_HERE", "requestData": "{\\"system\\":{\\"set_relay_state\\":{\\"state\\":1}}}" }}'
   response = requests.post('https://use1-wap.tplinkcloud.com/', headers=headers, params=params, data=data)
   m = 'Data sent to TPLink REST API' + data
   log(m)
@@ -63,10 +66,10 @@ def func2():
   }
 
   params = (
-    ('token', '93543fb3-71c246da834245d488b8a17'),
+    ('token', 'YOUR_TOKEN_HERE'),
   )
 
-  data = '{"method":"passthrough", "params": {"deviceId": "8006C228155C6C83E12C0B0706A5EC18184C53CE", "requestData": "{\\"system\\":{\\"set_relay_state\\":{\\"state\\":0}}}" }}'
+  data = '{"method":"passthrough", "params": {"deviceId": "YOUR_DEVICEID_HERE", "requestData": "{\\"system\\":{\\"set_relay_state\\":{\\"state\\":0}}}" }}'
   response = requests.post('https://use1-wap.tplinkcloud.com/', headers=headers, params=params, data=data)
   m = 'Data sent to TPLink REST API' + data
   log(m)
